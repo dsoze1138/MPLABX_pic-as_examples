73eeeea3bd92ba24e90e1ed7702b22f92fe50f3e
dsoze1138 <dsoze1138@gmail.com>
2021-02-22 07:10:56 -0800

    add build stamp

---
+++ 18F57Q43_bug_v545.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=18F57Q43_bug_v545.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/18F57Q43_bug_v545.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=18f57q43bugv545.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/18f57q43bugv545.x.tar
