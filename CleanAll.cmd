465a7a9398ee10f9bcff0eda1be48678d49f282c
dsoze1138 <dsoze1138@gmail.com>
2021-09-24 12:46:22 -0700

    Update cleaning scripts for MPLAB v8.92

--- CleanAll.cmd
+++ CleanAll.cmd
@@ -1,2 +1,7 @@
-@FOR /F  %%i IN ('dir /b *.x') DO @call CleanDir.cmd %%i
+@if not exist *.x goto NoMPLABX
+@FOR /F  %%i IN ('dir /b *.x') DO @call CleanDir.cmd %%i NoPause
+:NoMPLABX
+@if not exist *.8 goto NoMPLAB8
+@FOR /F  %%i IN ('dir /b *.8') DO @call CleanDir.cmd %%i NoPause
+:NoMPLAB8
 @pause
--- CleanDir.cmd
+++ CleanDir.cmd
@@ -1,6 +1,23 @@
+@rem This script cleans up after MPLABX or MPLAB 8.92 
+@rem development environments have been use to build 
+@rem a project in a folder.
+@rem
+@rem When creating an MPLAB v8.92 folder use ".8" as 
+@rem the last two character of the folder name.
+@rem
+@rem This script can be used with drag-and-drop or
+@rem invoked from a script like:
+@rem
+@rem @FOR /F  %%i IN ('dir /b *.x') DO @call CleanDir.cmd %%i NoPause
+@rem @FOR /F  %%i IN ('dir /b *.8') DO @call CleanDir.cmd %%i NoPause
+@rem @pause
+@rem
 @if "%1."=="." goto End
-@pushd %1
+@set FOLDER2CLEAN=%1
+@set LAST_CHARS=%FOLDER2CLEAN:~-2%
+@pushd %FOLDER2CLEAN%
 @echo Cleaning folder: %1
+@if "%LAST_CHARS%."==".8." goto Clean8
 @if exist html              rmdir /s /q html
 @if exist nbproject\private rmdir /s /q nbproject\private
 @if exist debug             rmdir /s /q debug
@@ -14,5 +31,26 @@
 @if exist funclist          del   /f /q funclist
 @if exist nbproject\Package-*.bash del   /f /q nbproject\Package-*.bash
 @if exist nbproject\Makefile-* del   /f /q nbproject\Makefile-*
+@goto CleanDone
+:Clean8
+@if exist funclist          del   /f /q funclist
+@if exist *.cof             del   /f /q *.cof
+@if exist *.hxl             del   /f /q *.hxl
+@if exist *.map             del   /f /q *.map
+@if exist *.mcs             del   /f /q *.mcs
+@if exist *.obj             del   /f /q *.obj
+@if exist *.rlf             del   /f /q *.rlf
+@if exist *.sdb             del   /f /q *.sdb
+@if exist *.sym             del   /f /q *.sym
+@if exist *.p1              del   /f /q *.p1 
+@if exist *.pre             del   /f /q *.pre
+@if exist *.lst             del   /f /q *.lst
+@if exist *.hex             del   /f /q *.hex
+@if exist *.err             del   /f /q *.err
+@if exist *.o               del   /f /q *.o
+:CleanDone
 @popd
+@set FOLDER2CLEAN=
+@set LAST_CHARS=
+@if not "%2."=="NoPause." pause
 :End
